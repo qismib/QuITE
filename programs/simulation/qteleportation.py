4a54bb78c39ad3f3f254c6def581dd428a561da7
DarioTona <tonadario@gmail.com>
2021-04-30 21:54:46 +0200

    ho modificato i registri classici e ho aggiunto la funzione di misura di Alice

--- programs/simulation/qteleportation.py
+++ programs/simulation/qteleportation.py
@@ -12,28 +12,31 @@ from qiskit.extensions import Initialize
 provider = IBMQ.load_account()
 
 #configuaration
+Alice_cr0 = ClassicalRegister(1)
+Alice_cr1 = ClassicalRegister(1)
 qr = QuantumRegister(3)
-Alice_cr = ClassicalRegister(2)
-Bob_cr = ClassicalRegister(1)
-circuit = QuantumCircuit(qr, Alice_cr, Bob_cr)
+circuit = QuantumCircuit(qr, Alice_cr0, Alice_cr1)
 
 #trasformo il primo qubit in un generico stato |psi> che voglio teletrasportare
-create_a_random_state0(circuit, qr)
-#create_a_random_state1(circuit, qr)
+#create_a_random_state0(circuit, qr)
+create_a_random_state1(circuit, qr)
+
 
 #creo l'entanglement tra il secondo e terzo qubit
 Bell_pair(circuit, qr)
 
 #alice applica le sue porte e misura i suoi qubits
-Alice_operations(circuit, qr, Alice_cr)
+Alice_operations(circuit, qr)
+
+Alice_measurements(circuit, qr, Alice_cr0, Alice_cr1)
 
 print('Alice sta comunicando i risultati a Bob')
 
 #bob applica l'operazione corretta
-Bob_operations(circuit, qr, Alice_cr)
+Bob_operations(circuit, qr, Alice_cr0, Alice_cr1)
 
-#misura finale
-circuit.measure(qr[2], Bob_cr)
+#verifica
+#circuit.measure(qr[2]), Bob_cr)
 
 
 simulator = Aer.get_backend('qasm_simulator')
