748c4640e53059a45feae4a24b332c7e1e635d10
DarioTona <tonadario@gmail.com>
2021-05-09 20:27:57 +0200

    ho implementato una funzione di verifica

--- programs/simulation/qteleportation.py
+++ programs/simulation/qteleportation.py
@@ -14,8 +14,11 @@ provider = IBMQ.load_account()
 #configuaration
 Alice_cr0 = ClassicalRegister(1)
 Alice_cr1 = ClassicalRegister(1)
+Bob_cr = ClassicalRegister(1)
 qr = QuantumRegister(3)
-circuit = QuantumCircuit(qr, Alice_cr0, Alice_cr1)
+circuit = QuantumCircuit(qr, Alice_cr0, Alice_cr1, Bob_cr)
+
+
 
 #trasformo il primo qubit in un generico stato |psi> che voglio teletrasportare
 #create_a_random_state0(circuit, qr)
@@ -29,14 +32,15 @@ Bell_pair(circuit, qr)
 Alice_operations(circuit, qr)
 
 Alice_measurements(circuit, qr, Alice_cr0, Alice_cr1)
-
-print('Alice sta comunicando i risultati a Bob')
+circuit.barrier()
 
 #bob applica l'operazione corretta
 Bob_operations(circuit, qr, Alice_cr0, Alice_cr1)
 
 #verifica
-#circuit.measure(qr[2]), Bob_cr)
+checking(circuit, qr, Bob_cr)
+circuit.measure(qr[2], Bob_cr)
+
 
 
 simulator = Aer.get_backend('qasm_simulator')
